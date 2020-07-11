7d46dfd17d229b8f9cda23868f5804e0e1feaa04
James Fisher <jfisher396@hotmail.com>
2020-07-10 18:23:59 -0700

    worked out write to output function

--- app.js
+++ app.js
@@ -10,10 +10,24 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
+const { createInflate } = require("zlib");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-let managerArr = [];
+class Team {
+    constructor() {
+        this.teamSize = 0;
+        this.team = [];
+    }
+}
+const teamMembers = [];
+
+function start() {
+    managerQuery();
+    // addTeamMember();
+
+}
+console.log(teamMembers);
 
 function managerQuery() {
     inquirer.prompt([{
@@ -36,11 +50,12 @@ function managerQuery() {
                 name: "officeNumber",
                 message: "Team Manager's office number:"
             }
-        ])
-        .then(answers => {
-            // console.log(answers)
-            managerArr.push(answers.name)
-            // Use user feedback for... whatever!!
+        ]).then(val => {
+            const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
+            console.log(manager);
+            teamMembers.push(manager);
+            console.log(teamMembers);
+            addTeamMember();
         })
         .catch(error => {
             if (error.isTtyError) {
@@ -49,78 +64,114 @@ function managerQuery() {
                 // Something else when wrong
             }
         });
-        console.log(managerArr)
+    
 };
-managerQuery()
 
-// function engineerQuery() {
-//     inquirer.prompt([{
-//                 type: "input",
-//                 name: "name",
-//                 message: "Engineer's name?"
-//             },
-//             {
-//                 type: "input",
-//                 name: "id",
-//                 message: "Engineer's ID number:"
-//             },
-//             {
-//                 type: "input",
-//                 name: "email",
-//                 message: "Engineer's email address:"
-//             },
-//             {
-//                 type: "input",
-//                 name: "github",
-//                 message: "What is the URL of the Engineer's GitHub profile?"
-//             }
-//         ])
-//         .then(answers => {
-//             // Use user feedback for... whatever!!
-//         })
-//         .catch(error => {
-//             if (error.isTtyError) {
-//                 // Prompt couldn't be rendered in the current environment
-//             } else {
-//                 // Something else when wrong
-//             }
-//         });
-// };
+function addTeamMember() {
+    inquirer.prompt([{
+        type: "prompt",
+        name: "add team member",
+        message: "Would you like to add a team member?"
+    },
+    {
+        type: "list",
+        name: "what_type",
+        message: "Add engineer or intern?",
+        choices: ["Engineer", "Intern", "Not at this time"]
+    }
+    ]).then(val => {
+        switch (val.what_type) {
+            case "Engineer": 
+                engineerQuery()
+                break;
+            case "Intern":
+            internQuery()
+            default:
+                createFile()
+                break;
+        }
+    }) 
+}
 
-// function internQuery(answers) {
-//     inquirer.prompt([{
-//             type: "input",
-//             name: "name",
-//             message: "Intern's name?"
-//         },
-//         {
-//             type: "input",
-//             name: "id",
-//             message: "Intern's ID number:"
-//         },
-//         {
-//             type: "input",
-//             name: "email",
-//             message: "Intern's email address:"
-//         },
-//         {
-//             type: "input",
-//             name: "school",
-//             message: "What school does/did the intern attend?"
-//         }
-//     ])
-//     .then(answers => {
-//         console.log(answers)
-//             // Use user feedback for... whatever!!
-//         })
-//         .catch(error => {
-//             if (error.isTtyError) {
-//                 // Prompt couldn't be rendered in the current environment
-//             } else {
-//                 // Something else when wrong
-//             }
-//         });
-// };
+
+function engineerQuery() {
+    inquirer.prompt([{
+                type: "input",
+                name: "name",
+                message: "Engineer's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Engineer's ID number:"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Engineer's email address:"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is the URL of the Engineer's GitHub profile?"
+            }
+        ]).then(val => {
+                const engineer = new Engineeer(val.name, val.id, val.email, val.officeNumber);
+                console.log(engineer);
+                teamMembers.push(engineer);
+                addTeamMember();
+            })
+            .catch(error => {
+                if (error.isTtyError) {
+                    // Prompt couldn't be rendered in the current environment
+                } else {
+                    // Something else when wrong
+                }
+            });
+        
+};
+
+function internQuery() {
+    inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "Intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Intern's ID number:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Intern's email address:"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school does/did the intern attend?"
+        }
+    ]).then(val => {
+            const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
+            console.log(manager);
+        })
+        .catch(error => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else when wrong
+            }
+        });
+    
+};
+function createFile() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "UTF-8");
+}
+start();
 
 // const employee1 = new Manager("Dave", "666", "dave@company.com", "123-456-7890");
 // const employee2 = new Engineer("Lucy", "123", "lucy@company.com", "lucy123");
