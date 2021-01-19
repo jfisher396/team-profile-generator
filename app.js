4abdff0dfa7fda197d0df446b8815e957f3f6158
James Fisher <jfisher396@hotmail.com>
2021-01-19 12:54:08 -0800

    updated bootstrap to version 5.0.0

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
 
 // Code to use inquirer to gather information about the development team members,
 // and to create objects for each team member.
