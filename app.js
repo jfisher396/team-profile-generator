14afcb15b599dc9078a21253554effd7e322fb6f
James Fisher <jfisher396@hotmail.com>
2021-01-19 12:23:21 -0800

    removed unused code

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
-const {
-    createInflate
-} = require("zlib");
 
 // Code to use inquirer to gather information about the development team members,
 // and to create objects for each team member.
@@ -45,6 +42,7 @@ function managerQuery() {
         }
     ]).then(val => {
         const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
+        console.log(manager)
         teamMembers.push(manager);
         addTeamMember();
     })
@@ -136,30 +134,5 @@ function createFile() {
     }
     fs.writeFileSync(outputPath, render(teamMembers), "UTF-8");
 }
+
 start();
-
-// const employee1 = new Manager("Dave", "666", "dave@company.com", "123-456-7890");
-// const employee2 = new Engineer("Lucy", "123", "lucy@company.com", "lucy123");
-// const employee3 = new Intern("Dan", "425", "dan@company.com", "UW");
-
-
-// console.log(employee1)
-// console.log(employee2)
-// console.log(employee3)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
