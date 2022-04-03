d689991c29128eff43d4a4b8c729f32b60118a98
James Fisher <jfisher396@hotmail.com>
2022-04-03 07:36:55 -0500

    formatted code

--- app.js
+++ app.js
@@ -15,123 +15,135 @@ const render = require("./lib/htmlRenderer");
 const teamMembers = [];
 
 function start() {
-    managerQuery();
+  managerQuery();
 }
 
 function managerQuery() {
-    inquirer.prompt([{
-            type: "input",
-            name: "name",
-            message: "What is the name of the team manager?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Team Manager's ID number:"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Team Manager's email address:"
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Team Manager's office number:"
-        }
-    ]).then(val => {
-        const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
-        console.log(manager)
-        teamMembers.push(manager);
-        addTeamMember();
-    })
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the team manager?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Team Manager's ID number:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Team Manager's email address:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Team Manager's office number:",
+      },
+    ])
+    .then((val) => {
+      const manager = new Manager(
+        val.name,
+        val.id,
+        val.email,
+        val.officeNumber
+      );
+      console.log(manager);
+      teamMembers.push(manager);
+      addTeamMember();
+    });
+}
 
 function addTeamMember() {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         type: "list",
         name: "what_type",
         message: "Add an engineer or intern to the team?",
-        choices: ["Engineer", "Intern", "Not at this time"]
-    }]).then(val => {
-
-        if (val.what_type === "Engineer") {
-            engineerQuery();
-        } else if (val.what_type === "Intern") {
-            internQuery();
-        } else {
-            createFile();
-        }
-    })
+        choices: ["Engineer", "Intern", "Not at this time"],
+      },
+    ])
+    .then((val) => {
+      if (val.what_type === "Engineer") {
+        engineerQuery();
+      } else if (val.what_type === "Intern") {
+        internQuery();
+      } else {
+        createFile();
+      }
+    });
 }
 
-
 function engineerQuery() {
-    inquirer.prompt([{
-            type: "input",
-            name: "name",
-            message: "Engineer's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Engineer's ID number:"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Engineer's email address:"
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "What is the URL of the Engineer's GitHub profile?"
-        }
-    ]).then(val => {
-        const engineer = new Engineer(val.name, val.id, val.email, val.github);
-        console.log(engineer);
-        teamMembers.push(engineer);
-        addTeamMember();
-    })
-
-
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Engineer's ID number:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Engineer's email address:",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the URL of the Engineer's GitHub profile?",
+      },
+    ])
+    .then((val) => {
+      const engineer = new Engineer(val.name, val.id, val.email, val.github);
+      console.log(engineer);
+      teamMembers.push(engineer);
+      addTeamMember();
+    });
+}
 
 function internQuery() {
-    inquirer.prompt([{
-            type: "input",
-            name: "name",
-            message: "Intern's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Intern's ID number:"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Intern's email address:"
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "What school does/did the intern attend?"
-        }
-    ]).then(val => {
-        const intern = new Intern(val.name, val.id, val.email, val.school);
-        teamMembers.push(intern);
-        addTeamMember();
-    })
-
-
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Intern's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Intern's ID number:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Intern's email address:",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school does/did the intern attend?",
+      },
+    ])
+    .then((val) => {
+      const intern = new Intern(val.name, val.id, val.email, val.school);
+      teamMembers.push(intern);
+      addTeamMember();
+    });
+}
 
 function createFile() {
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR)
-    }
-    fs.writeFileSync(outputPath, render(teamMembers), "UTF-8");
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(teamMembers), "UTF-8");
 }
 
 start();
